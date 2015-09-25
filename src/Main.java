4ddce0c6a5558480548f57948486c6d21f73e9e6
Сандро <mamutiks@bigmir.net>
2015-09-25 04:38:39 +0300

    Изменения_3

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,37 @@ import java.util.Arrays;
  */
 public class Main {
 
+    private static final int[] array_1 = {1, 5, 4, 23, 65, 32, 78};
+    private static final int[] array_2 = {3, 5, 24, 4, 1, 2, 34, 45, 32, 5};
+    private static final int[] array_3 = {3, 5, 24, 54, 1, 2, 34, 45, 32};
+
     public static void main(String[] args) {
 
         MyArraysUtil arraysUtil = new MyArraysUtil();
 
-        int[] result;
+        int[] resultLeftUnion;
+        int[] resultMerge;
+        int[] resultInnerUnion;
+        int[] resultOuterUnion;
 
-        result = arraysUtil.leftUnion(arraysUtil.getArray_1(), arraysUtil.getArray_2());
-        System.out.println(Arrays.toString(result));
+        System.out.println("array_1 = " + Arrays.toString(array_1));
+        System.out.println("array_2 = " + Arrays.toString(array_2));
+        System.out.println("array_3 = " + Arrays.toString(array_3));
 
-        result = arraysUtil.merge(arraysUtil.getArray_1(), arraysUtil.getArray_2());
-        System.out.println(Arrays.toString(result));
+        resultLeftUnion = arraysUtil.leftUnion(array_1, array_2);
+        System.out.println("resultLeftUnion = " + Arrays.toString(resultLeftUnion));
+
+        resultMerge = arraysUtil.merge(array_1, array_3);
+        System.out.println("resultMerge = " + Arrays.toString(resultMerge));
+
+        resultInnerUnion = arraysUtil.innerUnion(array_1, array_2);
+        System.out.println("resultInnerUnion = " + Arrays.toString(resultInnerUnion));
+
+        resultOuterUnion = arraysUtil.outerUnion(array_1, array_2);
+        System.out.println("resultOuterUnion = " + Arrays.toString(resultOuterUnion));
     }
+
+
 
 
 }
